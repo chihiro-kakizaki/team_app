47f0d759187275cb3faeb3d16f63fd7cae431fb7
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-14 14:37:53 +0900

    add:Teamに所属しているUserの削除（離脱）は、そのTeamのオーナーか、そのUser自身しかできないように設定

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -27,10 +27,12 @@ class AssignsController < ApplicationController
   end
 
   def assign_destroy(assign, assigned_user)
-    if assigned_user == assign.team.owner
-      I18n.t('views.messages.cannot_delete_the_leader')
+    if [assign.team.owner, assigned_user].exclude?(current_user)
+      I18n.t('views.messages.cannot_delete_the_member')
+    elsif assigned_user == assign.team.owner
+      I18n.t('views.messages.cannot_delete_the_leader') 
     elsif Assign.where(user_id: assigned_user.id).count == 1
-      I18n.t('views.messages.cannot_delete_only_a_member')
+      I18n.t('views.messages.cannot_delete_only_a_member') 
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
